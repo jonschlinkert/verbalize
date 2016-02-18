0e4b2d43dddb7cd2e9b3980a53834b755d91fd18
Brian Woodward <brian.woodward@gmail.com>
2016-02-17 22:49:22 -0500

    example using newer emitting code

---
+++ foo.js
@@ -0,0 +1,60 @@
+// 'use strict';
+var argv = require('minimist')(process.argv.slice(2), {
+  alias: {
+    v: 'verbose'
+  },
+  boolean: ['verbose']
+});
+
+var utils = require('./lib/utils');
+var Verbalize = require('./');
+
+var logger = new Verbalize(utils.extend({verbose: false}, argv));
+logger.define('isEnabled', function(modes) {
+  if (!modes) return true;
+  var comparison = true;
+  var mode = modes.split('.').filter(function(m) {
+    if (m === 'not') {
+      comparison = !comparison;
+      return false;
+    }
+    return true;
+  })[0];
+
+  if (!mode) return comparison;
+  return this.options[mode] === comparison;
+});
+
+// logger modes
+logger.modes(['verbose', 'not']);
+
+// logger methods
+logger.create('info');
+logger.create('warn');
+logger.create('error');
+logger.create('success');
+logger.create('write');
+logger.create('writeln');
+
+logger.on('info', function(stats) {
+  if (this.isEnabled(stats.mode)) {
+    logger.writeln('%j', stats);
+  }
+});
+
+logger
+  .info('this is a normal info message')
+  .verbose.info('this is a verbose message')
+  .not.verbose.info('this is a not.verbose message')
+  .not.not.verbose.info('this is a not.not.verbose message')
+  .not.verbose.not.info('this is a not.verbose.not message')
+  .writeln();
+
+// logger
+//   .verbose.error('--- VERBOSE INFO---').notverbose.subhead('--- IMPORTANT INFO ---')
+//   .verbose.inform('inform')
+//   .verbose.info('info')
+//   .verbose.warn('warn')
+//   .verbose.error('error').notverbose.error('error')
+//   .verbose.success('success').notverbose.success('success')
+//   .writeln();
