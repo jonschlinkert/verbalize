103ae16d58537ad2d1757eae63248847f536c029
Brian Woodward <brian.woodward@gmail.com>
2016-02-17 23:21:48 -0500

    use colors and styles plugin process helper method for determining if something should be logged and stylizing the output

--- foo.js
+++ foo.js
@@ -8,8 +8,12 @@ var argv = require('minimist')(process.argv.slice(2), {
 
 var utils = require('./lib/utils');
 var Verbalize = require('./');
+var colors = require('./plugins/colors');
+var styles = require('./plugins/styles');
 
 var logger = new Verbalize(utils.extend({verbose: false}, argv));
+logger.use(colors());
+logger.use(styles());
 logger.define('isEnabled', function(modes) {
   if (!modes) return true;
   var comparison = true;
@@ -25,34 +29,36 @@ logger.define('isEnabled', function(modes) {
   return this.options[mode] === comparison;
 });
 
-// logger modes
-logger.modes(['verbose', 'not']);
-
-// logger methods
-logger.create('info');
-logger.create('warn');
-logger.create('error');
-logger.create('success');
-
-logger.on('info', function(stats) {
+logger.define('process', function(stats) {
   if (this.isEnabled(stats.mode)) {
-    logger.writeln('%j', stats);
+    var res = stats.style.reduce(function(acc, style) {
+      acc = this.stylize(style, acc);
+      return acc;
+    }.bind(this), stats.args);
+    this.writeln.apply(this, res);
   }
 });
 
+// logger modes
+logger.modes(['verbose', 'not']);
+
+logger.on('*', function(stats) {
+  this.process(stats);
+});
+
 logger
-  .info('this is a normal info message')
-  .verbose.info('this is a verbose message')
-  .not.verbose.info('this is a not.verbose message')
-  .not.not.verbose.info('this is a not.not.verbose message')
-  .not.verbose.not.info('this is a not.verbose.not message')
+  .info('this is a normal info message') // always logged
+  .verbose.info('this is a verbose message') // logged when `options.verbose === true`
+  .not.verbose.info('this is a not.verbose message') // logged when `options.verbose === false`
+  .not.not.verbose.info('this is a not.not.verbose message') // logged when `options.verbose === true`
+  .not.verbose.not.info('this is a not.verbose.not message') // logged when `options.verbose === true`
   .writeln();
 
-// logger
-//   .verbose.error('--- VERBOSE INFO---').notverbose.subhead('--- IMPORTANT INFO ---')
-//   .verbose.inform('inform')
-//   .verbose.info('info')
-//   .verbose.warn('warn')
-//   .verbose.error('error').notverbose.error('error')
-//   .verbose.success('success').notverbose.success('success')
-//   .writeln();
+logger
+  .verbose.error('--- VERBOSE INFO---').not.verbose.subhead('--- IMPORTANT INFO ---')
+  .verbose.inform('inform')
+  .verbose.info('info')
+  .verbose.warn('warn')
+  .verbose.error('error').not.verbose.error('error')
+  .verbose.success('success').not.verbose.success('success')
+  .writeln();
