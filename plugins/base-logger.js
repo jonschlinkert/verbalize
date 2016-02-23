658c1d3787fa2503e0a5a7e428f0632d9631c1e0
Brian Woodward <brian.woodward@gmail.com>
2016-02-22 21:14:51 -0500

    update base-logger plugin

--- plugins/base-logger.js
+++ plugins/base-logger.js
@@ -4,28 +4,58 @@ var Verbalize = require('../');
 var utils = require('../lib/utils');
 
 module.exports = function(options) {
+  var opts = utils.extend({defaultListener: true}, options);
+
   return function(app) {
     if (typeof this['logger'] !== 'undefined') return;
     var logger = new Verbalize();
     logger.options = app.options;
+    logger.on('addLogger', addMethod);
+    logger.on('addMode', addMethod);
+
+    addMethods(logger.modifiers);
+    addMethods(logger.modes);
+
+    // defaults
+    logger.addMode('verbose');
+    logger.addMode('not', {type: 'toggle'});
+
+    if (opts.defaultListener === true) {
+      logger.on('*', function(name, stats) {
+        this.handle(stats);
+      });
+    }
+
+    this.define('logger', {
+      enumerable: true,
+      configurable: true,
+      get: function() {
+        function fn() {
+          var args = [].slice.call(arguments);
+          args.unshift('log');
+          return logger._emit.apply(logger, args);
+        }
+        fn.__proto__ = logger;
+        return fn;
+      }
+    });
 
     function addMethod(name) {
       app.define(name, {
         enumerable: true,
         configurable: true,
         get: function() {
-          return this.logger[name];
+          return logger[name];
         }
       });
     }
 
-    logger.on('addLogger', addMethod);
-    logger.on('addMode', addMethod);
-
-    this.define('logger', function() {
-      return logger.log.apply(logger, args);
-    });
-
-    this.logger.__proto__ = logger;
+    function addMethods(obj) {
+      var methods = Object.keys(obj);
+      var len = methods.length, i = -1;
+      while(++i < len) {
+        addMethod(methods[i]);
+      }
+    }
   };
 };
